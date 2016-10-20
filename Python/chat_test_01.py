b1f528a5f3c9249352ed931c80cc19a465c02f1b
Matthew French <mfrench71@googlemail.com>
2016-10-20 14:46:55 +0100

    External stop word list

--- Python/chat_test_01.py
+++ Python/chat_test_01.py
@@ -11,33 +11,11 @@ thirdConversation = True
 result = ""
 
 # Define stop words list
+# Read extenal file and into list and strip newlines
 
-stopWords = ["a", "a's", "able", "about", "above", "abroad", "according", "accordingly", \
-             "across", "actually", "adj", "after", "afterwards", "again", "against", "ago", \
-             "ahead", "ain't", "aint", "all", "allow", "allows", "almost", "alone", "along",\
-             "alongside", "already", "also", "although", "always", "am", "amid", "amidst", \
-             "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anybody", \
-             "anyhow", "anyone", "anything", "anyway", "anyways", "anywhere", "apart", "appear", \
-             "appreciate", "appropriate", "are", "aren't", "arent", "around", "as", "aside", "ask", \
-             "asking", "associated", "at", "available", "away", "awfully", "b", "back", "backward", \
-             "backwards", "be", "became", "because", "become", "becomes", "becoming", "been", \
-             "before", "beforehand", "begin", "behind", "being", "believe", "below", "beside", \
-             "besides", "best", "better", "between", "beyond", "bill", "both", "bottom", "brief", \
-             "but", "by", "c", "c'mon", "c's", "call", "came", "can", "can't", "cannot", "cant", \
-             "caption", "cause", "causes", "certain", "certainly", "changes", "clearly", "cmon", \
-             "co", "co.", "com", "come", "comes", "computer", "con", "concerning", "consequently",\
-             "consider", "considering", "contain", "containing", "contains", "corresponding", "could",\
-             "couldn't", "couldnt", "course", "cry", "currently", "d", "dare", "daren't", "darent", \
-             "de", "definitely", "describe", "described", "despite", "detail", "did", "didn't", \
-             "didnt", "different", "directly", "do", "does", "doesn't", "doesnt", "doing", "don't", \
-             "done", "dont", "down", "downwards", "due", "during", "e", "each", "edu", "eg", "eight",\
-             "eighty", "either", "eleven", "else", "elsewhere", "empty", "end", "ending", "enough",\
-             "entirely", "especially", "et", "etc", "even", "ever", "evermore", "every", "everybody", \
-             "everyone", "everything", "everywhere", "ex", "exactly", "example", "except", "f",\
-             "fairly", "far", "farther", "few", "fewer", "fifteen", "fifth", "fify", "fill", "find", \
-             "fire", "first", "five", "followed", "following", "follows", "for", "forever", "former",\
-             "formerly", "forth", "forty", "forward", "found", "four", "from", "front", "full", "further", "furthermore", "g", "get", "gets", "getting", "give", "given", "gives", "go", "goes", "going", "gone", "got", "gotten", "greetings", "h", "had", "hadn't", "hadnt", "half", "happens", "hardly", "has", "hasn't", "hasnt", "have", "haven't", "havent", "having", "he", "he'd", "he'll", "he's", "hello", "help", "hence", "her", "here", "here's", "hereafter", "hereby", "herein", "heres", "hereupon", "hers", "herse", "herself", "hi", "him", "himse", "himself", "his", "hither", "hopefully", "how", "how's", "howbeit", "however", "hows", "hundred", "i", "i'd", "i'll", "i'm", "i've", "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc", "inc.", "indeed", "indicate", "indicated", "indicates", "inner", "inside", "insofar", "instead", "interest", "into", "inward", "is", "isn't", "isnt", "it", "it'd", "it'll", "it's", "itd", "itll", "its", "itse", "itself", "j", "just", "k", "keep", "keeps", "kept", "know", "known", "knows", "l", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "let's", "lets", "like", "liked", "likely", "likewise", "little", "look", "looking", "looks", "low", "lower", "ltd", "m", "made", "mainly", "make", "makes", "many", "may", "maybe", "mayn't", "maynt", "me", "mean", "meantime", "meanwhile", "merely", "might", "mightn't", "mightnt", "mill", "mine", "minus", "miss", "more", "moreover", "most", "mostly", "move", "mr", "mrs", "much", "must", "mustn't", "mustnt", "my", "myse", "myself", "n", "name", "namely", "nd", "near", "nearly", "necessary", "need", "needn't", "neednt", "needs", "neither", "never", "neverf", "neverless", "nevertheless", "new", "next", "nine", "ninety", "no", "no-one", "nobody", "non", "none", "nonetheless", "noone", "nor", "normally", "not", "nothing", "notwithstanding", "novel", "now", "nowhere", "o", "obviously", "of", "off", "often", "oh", "ok", "okay", "old", "on", "once", "one", "one's", "ones", "only", "onto", "opposite", "or", "other", "others", "otherwise", "ought", "oughtn't", "oughtnt", "our", "ours", "ourselves", "out", "outside", "over", "overall", "own", "p", "part", "particular", "particularly", "past", "per", "perhaps", "placed", "please", "plus", "possible", "presumably", "probably", "provided", "provides", "put", "q", "que", "quite", "qv", "r", "rather", "rd", "re", "really", "reasonably", "recent", "recently", "regarding", "regardless", "regards", "relatively", "respectively", "right", "round", "s", "said", "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "shall", "shan't", "shant", "she", "she'd", "she'll", "she's", "shes", "should", "shouldn't", "shouldnt", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somebody", "someday", "somehow", "someone", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify", "specifying", "still", "sub", "such", "sup", "sure", "system", "t", "t's", "take", "taken", "taking", "tell", "ten", "tends", "th", "than", "thank", "thanks", "thanx", "that", "that'll", "that's", "that've", "thatll", "thats", "thatve", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "there'd", "there'll", "there're", "there's", "there've", "thereafter", "thereby", "thered", "therefore", "therein", "therell", "therere", "theres", "thereupon", "thereve", "these", "they", "they'd", "they'll", "they're", "they've", "theyd", "theyll", "theyre", "theyve", "thick", "thin", "thing", "things", "think", "third", "thirty", "this", "thorough", "thoroughly", "those", "though", "three", "through", "throughout", "thru", "thus", "till", "to", "together", "too", "took", "top", "toward", "towards", "tried", "tries", "truly", "try", "trying", "twelve", "twenty", "twice", "two", "u", "un", "under", "underneath", "undoing", "unfortunately", "unless", "unlike", "unlikely", "until", "unto", "up", "upon", "upwards", "us", "use", "used", "useful", "uses", "using", "usually", "v", "value", "various", "versus", "very", "via", "viz", "vs", "w", "want", "wants", "was", "wasn't", "wasnt", "way", "we", "we'd", "we'll", "we're", "we've", "welcome", "well", "went", "were", "weren't", "werent", "weve", "what", "what'll", "what's", "what've", "whatever", "whatll", "whats", "whatve", "when", "when's", "whence", "whenever", "whens", "where", "where's", "whereafter", "whereas", "whereby", "wherein", "wheres", "whereupon", "wherever", "whether", "which", "whichever", "while", "whilst", "whither", "who", "who'd", "who'll", "who's", "whod", "whoever", "whole", "wholl", "whom", "whomever", "whos", "whose", "why", "why's", "whys", "will", "willing", "wish", "with", "within", "without", "won't", "wonder", "wont", "would", "wouldn't", "wouldnt", "x", "y", "yes", "yet", "you", "you'd", "you'll", "you're", "you've", "youd", \
-             "youll", "your", "youre", "yours", "yourself", "yourselves", "youve", "z", "zero"]
+with open ('stopwords.txt','r') as f:
+    stopWords = [x.strip('\n') for x in f.readlines()]
+    #print(stopWords)
 
 # Define other lists, prompts and responses
 
@@ -147,8 +125,10 @@ except:
     for i, topic in enumerate(topics):
         print (i, unidecode(topic))
     choice = int(raw_input("Choose one ..."))
-    assert choice in xrange (len(topics))
-    print unidecode(wikipedia.summary(topics[choice], sentences = 3))
+    if choice in xrange (len(topics)):
+        print unidecode(wikipedia.summary(topics[choice], sentences = 3))
+    else:
+        print("Oops")
 
 print(divider)
 time.sleep(3)
@@ -167,16 +147,24 @@ time.sleep(3)
 # Handle disambiguation
 
 try: 
-    print unidecode(wikipedia.summary(query, sentences = 1))
+    print unidecode(wikipedia.summary(query, sentences = 2))
 except:
     topics = wikipedia.search(query)
     print ("Did you mean? ...")
+    # Loop through search results with count/values
     for i, topic in enumerate(topics):
+        # Print numbered list of results
         print (i, unidecode(topic))
+    # Prompt user to choose an item
     choice = int(raw_input("Choose one ..."))
-    assert choice in xrange (len(topics))
-    print unidecode(wikipedia.summary(topics[choice], sentences = 1))
-    
+    # Check option chosen is included in available options
+    if choice in xrange (len(topics)):
+        # If it is, print the Wikipedia summary for chosen option
+        print unidecode(wikipedia.summary(topics[choice], sentences = 2))
+    # Otherwise, error
+    else:
+        print("Oops")
+        
 # Age
 
 print(divider)
